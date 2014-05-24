5227c0b97392a96428fb1e5ed3b1b9f41f2464d2
DougNoGit <dougsmcgirr@gmail.com>
2014-05-23 22:53:46 -0700

    menu added for slides 2 through 5

--- gnenepalapp/src/main/java/com/yadaming/gnenepalapp/Slide3Activity.java
+++ gnenepalapp/src/main/java/com/yadaming/gnenepalapp/Slide3Activity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class Slide3Activity extends Activity {
 
@@ -22,10 +25,89 @@ public class Slide3Activity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.slide3, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.initial, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        super.onOptionsItemSelected(item);
+        /* a switch case for the item selected from the menu. */
+        switch(item.getItemId())
+        {
+            case R.id.action_slide1:
+                if (!SoundModule.playing) // if the audio is not playing then the next method can work
+                {
+                    Intent intent1 = new Intent(this, InitialActivity.class);
+                    startActivity(intent1);
+                    finish();
+                }
+                else if (SoundModule.playing) // if the audio is playing: tell the user to wait
+                {
+                    Toast.makeText(getBaseContext(), "You have to hear this!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.action_slide2:
+                if (!SoundModule.playing) // if the audio is not playing then the next method can work
+                {
+                    Intent intent2 = new Intent(this, Slide2Activity.class);
+                    startActivity(intent2);
+                    finish();
+                }
+                else if (SoundModule.playing) // if the audio is playing: tell the user to wait
+                {
+                    Toast.makeText(getBaseContext(), "You have to hear this!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.action_slide3:
+                if (!SoundModule.playing)
+                {
+                    Intent intent3 = new Intent(this, Slide3Activity.class);
+                    startActivity(intent3);
+                    finish();
+                }
+                else if (SoundModule.playing) // if the audio is playing: tell the user to wait
+                {
+                    Toast.makeText(getBaseContext(), "You have to hear this!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.action_slide4:
+                if (!SoundModule.playing)
+                {
+                    Intent intent4 = new Intent(this, Slide4Activity.class);
+                    startActivity(intent4);
+                    finish();
+                }
+                else if (SoundModule.playing) // if the audio is playing: tell the user to wait
+                {
+                    Toast.makeText(getBaseContext(), "You have to hear this!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.action_slide5:
+                if (!SoundModule.playing)
+                {
+                    Intent intent5 = new Intent(this, Slide5Activity.class);
+                    startActivity(intent5);
+                    finish();
+                }
+                else if (SoundModule.playing) // if the audio is playing: tell the user to wait
+                {
+                    Toast.makeText(getBaseContext(), "You have to hear this!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
 }
